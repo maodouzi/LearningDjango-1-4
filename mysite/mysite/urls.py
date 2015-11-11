5024b55294f484543626361121f4294a64c4c2b3
Wu Wenxiang <wu.wenxiang@99cloud.net>
2015-11-11 12:02:53 +0800

    Step7: Post Methods-Form

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from mysite.views import *
+from mysite.contact.views import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,9 +3,6 @@ import datetime
 from django.shortcuts import render
 from books.models import Publisher
 from mysite.books.models import Book
-from django.core.mail import send_mail
-from django.http import HttpResponseRedirect
-
 
 def hello(request):
     return HttpResponse("Hello world")
@@ -55,29 +52,5 @@ def search(request):
                 {'books': books, 'query': q})
     return render(request, 'search_form.html',
         {'errors': errors})
-    
-def contact(request):
-    errors = []
-    if request.method == 'POST':
-        if not request.POST.get('subject', ''):
-            errors.append('Enter a subject.')
-        if not request.POST.get('message', ''):
-            errors.append('Enter a message.')
-        if request.POST.get('email') and '@' not in request.POST['email']:
-            errors.append('Enter a valid e-mail address.')
-        if not errors:
-#            send_mail(
-#                request.POST['subject'],
-#                request.POST['message'],
-#                request.POST.get('email', 'noreply@example.com'),
-#                ['siteowner@example.com'],
-#            )
-            return HttpResponseRedirect('/contact/thanks/')
-    return render(request, 'contact_form.html', {
-        'errors': errors,
-        'subject': request.POST.get('subject', ''),
-        'message': request.POST.get('message', ''),
-        'email': request.POST.get('email', ''),
-    })
 
 
