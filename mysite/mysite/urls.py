ba06f10af7f75d50c1184e85d435a1b50c2b82cd
Wu Wenxiang <wu.wenxiang@99cloud.net>
2015-11-11 11:59:06 +0800

    Step5: Add Admins

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from mysite.views import hello, my_homepage_view, current_datetime, hours_ahead, first_app
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     ('^hello/$', hello),
@@ -19,5 +19,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
